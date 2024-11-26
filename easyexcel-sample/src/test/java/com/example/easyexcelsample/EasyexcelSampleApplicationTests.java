9ed45292b6109d04d2a79f274e672dd15d55f6a2
liliwen <hellomephllw@163.com>
2024-11-26 21:59:42 +0800

    feature: 初始化easyexcel

---
+++ easyexcel-sample/src/test/java/com/example/easyexcelsample/EasyexcelSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyexcelsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyexcelSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
