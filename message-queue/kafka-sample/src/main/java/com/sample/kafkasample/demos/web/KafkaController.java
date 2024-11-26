bc9cc2d1d8782d40a868c64ba464f9bd526b8d4b
liliwen <hellomephllw@163.com>
2024-11-26 21:54:25 +0800

    初始化

---
+++ message-queue/kafka-sample/src/main/java/com/sample/kafkasample/demos/web/KafkaController.java
@@ -0,0 +1,15 @@
+package com.sample.kafkasample.demos.web;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @description:
+ * @author: liliwen
+ * @date: 2024-03-18
+ */
+@Controller
+public class KafkaController {
+
+
+
+}
