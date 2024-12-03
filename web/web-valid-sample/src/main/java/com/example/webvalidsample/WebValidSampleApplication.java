0e4be8748faa045713edc6d87f859354c609fa47
liliwen <hellomephllw@163.com>
2024-12-03 23:16:49 +0800

    feature: 初始化web valid sample

---
+++ web/web-valid-sample/src/main/java/com/example/webvalidsample/WebValidSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.webvalidsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebValidSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebValidSampleApplication.class, args);
+    }
+
+}
