5f12928d2b852e696371938ba55b1e36bdf3f799
liliwen <hellomephllw@163.com>
2024-12-03 23:18:57 +0800

    feature: 初始化jackson sample

---
+++ json/jackson-sample/src/main/java/com/example/jacksonsample/JacksonSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jacksonsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonSampleApplication.class, args);
+    }
+
+}
---
+++ json/jackson-sample/src/test/java/com/example/jacksonsample/JacksonSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jacksonsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacksonSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
